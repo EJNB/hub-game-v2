3511902fbea39540dcaffc3c1208210017e3e447
Javier <javiernieto1989@gmail.com>
2023-07-21 23:13:58 -0400

    Ship genres with the app

--- src/hooks/useGenres.ts
+++ src/hooks/useGenres.ts
@@ -1,6 +1,11 @@
-import { Genre } from "../interfaces/response.ts";
-import useData from "./useData.ts";
+// import { Genre } from "../interfaces/response.ts";
+// import useData from "./useData.ts";
+import genres from "../data/genres.ts";
 
-const useGenres = () => useData<Genre>("/genres");
+// version calling the server
+// const useGenres = () => useData<Genre>("/genres");
+
+// Static version
+const useGenres = () => ({ loading: false, error: null, data: genres });
 
 export default useGenres;
