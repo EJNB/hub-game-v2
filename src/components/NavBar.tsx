112045cbb31084acac7944a023c63a3add5cdc61
Javier <javiernieto1989@gmail.com>
2023-07-29 17:29:05 -0400

    Fix the navbar

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -2,11 +2,14 @@ import { HStack, Image } from "@chakra-ui/react";
 import ColorModeSwitch from "./ColorModeSwitch.tsx";
 import Logo from "../assets/logo.webp";
 import SearchInput from "./SearchInput.tsx";
+import { Link } from "react-router-dom";
 
 const NavBar = () => {
   return (
     <HStack>
-      <Image src={Logo} boxSize="60px"></Image>
+      <Link to="/">
+        <Image src={Logo} boxSize="60px" objectFit="cover" />
+      </Link>
       <SearchInput />
       <ColorModeSwitch />
     </HStack>
--- src/components/SearchInput.tsx
+++ src/components/SearchInput.tsx
@@ -2,17 +2,22 @@ import { Input, InputGroup, InputLeftElement } from "@chakra-ui/react";
 import { BsSearch } from "react-icons/bs";
 import { useRef } from "react";
 import useGameQueryStore from "../store.ts";
+import { useNavigate } from "react-router-dom";
 
 const SearchInput = () => {
   const inputRef = useRef<HTMLInputElement>(null);
   const setSearchText = useGameQueryStore((state) => state.setSearchText);
+  const navigate = useNavigate();
 
   return (
     <form
       action=""
       onSubmit={(event) => {
         event.preventDefault();
-        if (inputRef.current) setSearchText(inputRef.current.value);
+        if (inputRef.current) {
+          setSearchText(inputRef.current.value);
+          navigate("/");
+        }
       }}
       style={{ width: "100%" }}
     >
